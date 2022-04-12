ea1a7dd74af1771f610683830c78121ff925770a
MEINE1337 <meine23@yandex.ru>
2022-04-12 18:19:35 +0300

    test

--- git/Ozon1.go
+++ git/Ozon1.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 var n int
 var a [11]int
@@ -11,9 +14,12 @@ func main() {
 }
 
 func Vvod() int {
+	var nlow, nhigh float64
+	nlow = math.Pow(10, 10)
+	nhigh = math.Pow(10, 11)
 	fmt.Println("Vvedite nomer telephona:")
 	fmt.Scan(&n)
-	if n < 10^10 || n >= 10^11 {
+	if n < int(nlow) || n >= int(nhigh) {
 		fmt.Println("Nepravilno vveden nomer telephona")
 		fmt.Scan(&n)
 		fmt.Printf("Znachenie ravno %d\n", n)
